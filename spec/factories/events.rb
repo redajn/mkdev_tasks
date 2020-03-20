c660d57d2d3c5e10e456e442b56b3098fe80fbc9
Andrey Ognev <vengoshara@gmail.com>
2020-03-20 14:57:36 +0300

    test huund config

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -2,24 +2,24 @@
 
 FactoryBot.define do
   factory :event do
-    title { 'Anniversary event' }
-    description { 'Jast some event like u see early' }
-    location { 'St. Petersburg' }
-    start_time { '2020-02-01T08:00:00' }
-    end_time { '2020-02-01T20:00:00' }
-    organizer_email { 'irganizer@mail.com' }
-    organizer_telegram { '@organizer' }
-    link { 'http://goodevent.com' }
+    title { "Anniversary event" }
+    description { "Jast some event like u see early" }
+    location { "St. Petersburg" }
+    start_time { "2020-02-01T08:00:00" }
+    end_time { "2020-02-01T20:00:00" }
+    organizer_email { "irganizer@mail.com" }
+    organizer_telegram { "@organizer" }
+    link { "http://goodevent.com" }
 
     factory :invalid_event do
       title { nil }
-      description { 'Jast some event like u see early' }
-      location { 'St. Petersburg' }
-      start_time { '2020-02-01T08:00:00' }
-      end_time { '2020-02-01T20:00:00' }
-      organizer_email { 'irganizer@mail.com' }
-      organizer_telegram { '@organizer' }
-      link { 'http://goodevent.com' }
+      description { "Jast some event like u see early" }
+      location { "St. Petersburg" }
+      start_time { "2020-02-01T08:00:00" }
+      end_time { "2020-02-01T20:00:00" }
+      organizer_email { "irganizer@mail.com" }
+      organizer_telegram { "@organizer" }
+      link { "http://goodevent.com" }
     end
   end
 end
