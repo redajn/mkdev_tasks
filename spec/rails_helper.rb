0d520c77d6567202403c7792fd6700b89249b800
Andrey Ognev <vengoshara@gmail.com>
2020-04-18 23:00:29 +0300

    fixup! fixup! fixup! fixup! fixup! configured travis

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,14 +32,10 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[no-sandbox headless disable-gpu] }
-  )
+Capybara.register_driver :chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
 
-  Capybara::Selenium::Driver.new app,
-                                 browser:              :chrome,
-                                 desired_capabilities: capabilities
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
-Capybara.default_driver = :headless_chrome
+Capybara.default_driver = :chrome
