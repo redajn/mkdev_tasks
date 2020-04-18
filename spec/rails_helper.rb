552417fc23290055fe7f0371f641e66bf692f011
Andrey Ognev <vengoshara@gmail.com>
2020-04-18 21:36:46 +0300

    fixup! fixup! configured travis

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,4 +36,14 @@ Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu window-size=1280,2000 no-sandbox) }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
 Capybara.default_driver = :chrome
